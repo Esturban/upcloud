035e86844c5dd758f157fc12aad5156b3166c4b3
Esturban <info@evadvisory.ca>
2024-11-10 07:14:17 +0300

    Cache
    Adding a step to cache the access token
    
    Not my best work, still a work in progress

--- upcloud/config.py
+++ upcloud/config.py
@@ -5,6 +5,7 @@ import requests
 import json
 
 load_dotenv()
+
 class Config:
     CLIENT_ID = os.getenv('CLIENT_ID')
     CLIENT_SECRET = os.getenv('AZURE_VALUE')
@@ -17,10 +18,18 @@ class Config:
 
 def get_access_token():
     try:
+        #See if an access token is already cached
         with open(f'{Config.CACHED_TOKEN_PATH}', 'r') as f:
             tokens = json.load(f)
-            return tokens['access_token']
-    except FileNotFoundError:
+            access_token = tokens.get('access_token')
+            auth_code = tokens.get('auth_code')
+            #Determine validity
+            if access_token and is_valid_jwt(auth_code):
+                return access_token
+            else:
+                raise ValueError("Invalid access token format")
+    #Otherwise, get a new access token
+    except (FileNotFoundError, ValueError):
         auth_url = f"{Config.AUTHORITY_URL}?client_id={Config.CLIENT_ID}&response_type=code&redirect_uri={Config.REDIRECT_URI}&response_mode=query&scope={' '.join(Config.SCOPES)}"
         print(f"Go to the following URL to authorize the application: {auth_url}")
         auth_code = input("Enter the authorization code: ")
@@ -33,10 +42,17 @@ def get_access_token():
             'redirect_uri': Config.REDIRECT_URI,
             'scope': ' '.join(Config.SCOPES)
         }
-
+        #Authentication request
         response = requests.post(Config.TOKEN_URL, data=token_data, auth=HTTPBasicAuth(Config.CLIENT_ID, Config.CLIENT_SECRET))
         response.raise_for_status()
         tokens = response.json()
+        tokens['auth_code'] = auth_code
+        #Cache the access token
         with open(f'{Config.CACHED_TOKEN_PATH}', 'w') as f:
             json.dump(tokens, f)
         return tokens['access_token']
+
+
+def is_valid_jwt(token):
+    parts = token.split('.')   
+    return len(parts) > 3
