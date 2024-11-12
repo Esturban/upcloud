43c4e45ef280a4e4350b4609cf9208afffcf2517
Esturban <info@evadvisory.ca>
2024-11-12 07:51:15 +0300

    JWT Auth
    Using the appropriate decoder for JWT in the process of reauthenticating to the API with a cached token

--- upcloud/config.py
+++ upcloud/config.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from requests.auth import HTTPBasicAuth
 import requests
 import json
+import logging
+from jwt import decode, InvalidTokenError
 
 load_dotenv()
 
@@ -18,19 +20,20 @@ class Config:
 
 def get_access_token():
     try:
-        #See if an access token is already cached
-        with open(f'{Config.CACHED_TOKEN_PATH}', 'r') as f:
+        with open(Config.CACHED_TOKEN_PATH, 'r') as f:
             tokens = json.load(f)
             access_token = tokens.get('access_token')
             auth_code = tokens.get('auth_code')
-            #Determine validity
-            if access_token and is_valid_jwt(auth_code):
+            if access_token and is_valid_jwt(access_token):
                 return access_token
             else:
                 raise ValueError("Invalid access token format")
-    #Otherwise, get a new access token
-    except (FileNotFoundError, ValueError):
-        auth_url = f"{Config.AUTHORITY_URL}?client_id={Config.CLIENT_ID}&response_type=code&redirect_uri={Config.REDIRECT_URI}&response_mode=query&scope={' '.join(Config.SCOPES)}"
+    except (FileNotFoundError, ValueError) as e:
+        logging.error(f"Token retrieval error: {e}")
+        auth_url = (
+            f"{Config.AUTHORITY_URL}?client_id={Config.CLIENT_ID}&response_type=code&redirect_uri={Config.REDIRECT_URI}"
+            f"&response_mode=query&scope={' '.join(Config.SCOPES)}"
+        )
         print(f"Go to the following URL to authorize the application: {auth_url}")
         auth_code = input("Enter the authorization code: ")
 
@@ -42,17 +45,18 @@ def get_access_token():
             'redirect_uri': Config.REDIRECT_URI,
             'scope': ' '.join(Config.SCOPES)
         }
-        #Authentication request
         response = requests.post(Config.TOKEN_URL, data=token_data, auth=HTTPBasicAuth(Config.CLIENT_ID, Config.CLIENT_SECRET))
         response.raise_for_status()
         tokens = response.json()
         tokens['auth_code'] = auth_code
-        #Cache the access token
-        with open(f'{Config.CACHED_TOKEN_PATH}', 'w') as f:
+        with open(Config.CACHED_TOKEN_PATH, 'w') as f:
             json.dump(tokens, f)
         return tokens['access_token']
 
 
 def is_valid_jwt(token):
-    parts = token.split('.')   
-    return len(parts) > 3
+    try:
+        decode(token, options={"verify_signature": False})
+        return True
+    except InvalidTokenError:
+        return False
